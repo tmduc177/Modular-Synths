ed6f1d422eea873776c46e3468aa1ea2ed54ebc9
tmduc177 <tmduc177@gmail.com>
2024-03-06 19:42:41 +0700

    simple dial

--- dials_generator.js
+++ dials_generator.js
@@ -3,7 +3,7 @@ import { getRandomElement, getRandomInt } from "./helper-funcs.js";
 
 const dials = {
     "size_multiplier": [2, 3, 4, 5, 6, 8],
-    "indicator_shape": ["circle", "short_triangle", "long_triangle", "rectangle", "diamond"],
+    "indicator_shape": ["circle", "triangle", "rectangle", "diamond"],
     "markings_distance_multiplier": [1, 1.25, 1.5, 1.75],
     "markings_style": ["circle", "triangle", "rectangle", "connected_line", "perpendicular_line"],
     "markings_highlight": ["circle", "triangle", "rectangle", "perpendicular_line"],
@@ -23,16 +23,15 @@ export class SingleDial {
             markings_style: getRandomElement(dials.markings_style),
             markings_subdivisions: getRandomInt(32, 3),
             markings_highlight: Math.random() > 0.5 ? true : false,
-            radius: 0,
-            relative_center: 0
+            rel_center_coords: 0
         };
 
-        this.style.radius = this.style.size * grid_size / 2;
-        this.style.relative_center = grid_size * 2 + this.style.radius;
-        this.style.inner_border_radius = this.inner_border ? 0 : this.style.radius * 0.8;
+        this.radius = (this.style.size * grid_size) / 2;
+        this.inner_border_radius = this.inner_border ? 0 : this.radius * 0.8;
 
+        this.rel_center_coords = grid_size * 2 + this.radius;
+        this.center = new Point(this.rel_center_coords, this.rel_center_coords);
         this.indicator = false;
-        this.center = new Point(this.style.relative_center, this.style.relative_center);
     };
 
     draw(color) {
@@ -41,24 +40,56 @@ export class SingleDial {
     }
 
     drawBorder(color) {
-        var dialBorder = new Path.Circle(this.center, this.style.radius);
+        var dialBorder = new Path.Circle(this.center, this.radius);
         dialBorder.strokeColor = color;
+        dialBorder.strokeWidth = 2
 
         if (this.style.inner_border) {
-            var innerDialBorder = new Path.Circle(this.center, this.style.inner_border_radius);
+            var innerDialBorder = new Path.Circle(this.center, this.inner_border_radius);
             innerDialBorder.strokeColor = color;    
+            innerDialBorder.strokeWidth = 2
         };
     }
 
     drawIndicator(color) {
-        var indicator_size = this.grid_size / getRandomInt(3, 5)
+        var indicator_radius;
+        var center_offset = 0;
         switch (this.style.indicator_shape) {
+            case "triangle":
+                indicator_radius = this.radius >= 20 ? 10 : this.radius / getRandomInt(2,3);
+                this.indicator = new Path.RegularPolygon(this.center, 3, indicator_radius);
+                center_offset = (0.8 * this.radius) - indicator_radius;
+                break;
+            case "rectangle":
+                var rectangle_height = getRandomInt(2,3) * this.radius / 4;
+                var rectangle_width = this.radius / 5;
+                this.indicator = new Path.Rectangle(
+                    new Point(this.rel_center_coords - rectangle_width / 2, this.rel_center_coords - rectangle_height / 2),
+                    new Point(this.rel_center_coords + rectangle_width / 2, this.rel_center_coords + rectangle_height / 2)
+                );
+                center_offset = (0.75 * this.radius) - (rectangle_height / 2);
+                break;
+            case "diamond":
+                indicator_radius = Math.floor((this.grid_size / getRandomInt(2, 3)) * 10) / 10;
+                this.indicator = new Path.RegularPolygon(this.center, 4, indicator_radius);
+                this.indicator.rotate(45);
+                center_offset = this.radius * 0.5;
+                break;
+            // line is buggy, sometimes it appears and sometimes it doesnt, havent figured out why
+            // case "line":
+            //     indicator_radius = this.radius / 4
+            //     this.indicator = new Path();
+            //     this.indicator.add(this.center);
+            //     this.indicator.add(new Point(this.rel_center_coords, this.rel_center_coords - (indicator_radius * 4)));
+            //     break;
             default:
-                this.indicator = new Path.Circle(this.center, indicator_size);
+                indicator_radius = Math.floor((this.grid_size / getRandomInt(3, 5)) * 10) / 10;
+                this.indicator = new Path.Circle(this.center, indicator_radius);
+                center_offset = this.radius * 0.5;
                 break;
         };
         
-        this.indicator.position.y -= this.style.radius * 0.5;
+        this.indicator.position.y -= center_offset;
 
         if (this.style.indicator_fill) {
             this.indicator.fillColor = color;
@@ -66,6 +97,7 @@ export class SingleDial {
             this.indicator.strokeColor = color;
         };
 
+        this.indicator.strokeWidth = 2
         
     }
 };
